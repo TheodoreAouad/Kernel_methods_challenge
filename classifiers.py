20069fe6a3f81c441ef0e5406676aaf1cb0f7942
Theodore Aouad <theodoreaouad@gmail.com>
2019-03-25 17:01:53 +0100

    added knn to classifiers.py

--- classifiers.py
+++ classifiers.py
@@ -67,3 +67,43 @@ class KSVM():
 
     def predict_fromK(self, K, only_support = False):
         return self.predict_score_fromK(K, only_support=only_support) > 0
+
+
+class KNN():
+    '''K nearest-neighbors'''
+
+    def __init__(self):
+        self.kernel = None
+        self.gram = None
+        self.data = None
+        self.label = None
+
+    def train(self,X,y,kernel):
+        self.kernel = kernel
+        self.data = X
+        self.label = y
+
+        #gram = np.zeros((X.shape[0],X.shape[0]))
+        #for i in range(X.shape[0]):
+        #  for j in range(i,X.shape[0]):
+        #    gram[i,j] = kernel(X[i],X[j])
+        #    gram[j,i] = gram[i,j]
+        #self.gram = gram
+        return True
+
+    def predict_one(self,x,k):
+        dists = np.zeros(self.data.shape[0])
+        for i in range(dists.shape[0]):
+            dists[i] = self.Kdist(x,self.data[i])
+        neighbors = self.label[dists.argsort()][:k]
+        uniq,cnts = np.unique(neighbors, return_counts=1)
+        return uniq[cnts.argsort()][-1]
+
+    def predict(self,X,k):
+        labels = np.zeros(X.shape[0])
+        for i,x in enumerate(X):
+            labels[i] = self.predict_one(x,k)
+        return labels
+
+    def Kdist(self,x,y):
+        return (self.kernel(x,x) + self.kernel(y,y) - 2*self.kernel(x,y))**0.5

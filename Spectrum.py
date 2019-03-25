5daa224c9b1154d8b690bd724b8cb3e6531bbb6b
Josselin <josselinbonnevie@yahoo.fr>
2019-03-25 11:05:34 +0100

    Spectrum Kernel + utils

---
+++ Spectrum.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+class spectrum():
+
+    def __init__(self, k = 3):
+            self.K = 3
+            self.N = None
+            self.pre_index = None
+            self.Graam = None
+
+    def compute_pre_index(self, X):
+        self.N = X.shape[0]
+        self.Graam = np.zeros((self.N,self.N))
+        pre_index = {}
+        for i in range(self.N):
+            seq = X[i]
+            l = len(seq)
+            for j in range(l-self.K):
+                subseq = seq[j:j+self.K]
+                if subseq in pre_index.keys():
+                    pre_index[subseq][i] +=1
+                else :
+                    new = np.zeros(self.N)
+                    new[i] = 1
+                    pre_index[subseq] = new
+        self.pre_index = pre_index
+
+    def compute_Graam(self, X):
+        self.compute_pre_index(X)
+        for sub_seq in self.pre_index:
+            V = self.pre_index[sub_seq]
+            self.Graam += np.outer(V,V)
+        return True
+
+    def compute_embedding(self, Xtest):
+        n = Xtest.shape[0]
+        Mat = np.zeros((n, self.N))
+        for i in range(n):
+            seq = Xtest[i]
+            l = len(seq)
+            for j in range(l-self.K):
+                Mat[i] += self.pre_index[subseq]
+        return Mat
+
+
+# X= Xtr0
+# n = Xtr0.shape[0]
+# k = 3
+#
---
+++ utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+import math
+
+
+
+def train_val_split(X,y, p=0.1):
+    l = len(X)
+    sep = math.floor(l*p)
+    Xtr = X[:-sep]
+    ytr = y[:-sep]
+    Xval = X[sep:]
+    yval = y[sep:]
+    return Xtr, ytr, Xval, yval
